044ad81109dc396a9c65e4f644bd7be54d4b82b0
AndreaCavallo <andreacavallossl@gmail.com>
2018-05-18 18:25:31 +0200

    Cambiamenti vari

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name: "Jack Jones",
+			email: "jackjones@gmail.com",
+			password:
+			"foobar",
+			password_confirmation: "foobar")
+
+99.times do |n|
+	name = Faker::Name.name
+	email = "utente-#{n+1}@gmail.com"
+	password = "password"
+	User.create!(name: name, email: email, password: password, password_confirmation: password)
+end
