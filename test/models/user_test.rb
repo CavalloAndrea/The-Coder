bc5b52f5d046f2206ef4ea9709d5473a56dd07d2
AndreaCavallo <andreacavallossl@gmail.com>
2018-05-24 18:17:54 +0200

    Concluso following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -76,4 +76,33 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
+  test "should follow and unfollow a user" do
+    prova = users(:prova)
+    archer = users(:archer)
+    assert_not prova.following?(archer)
+    prova.follow(archer)
+    assert prova.following?(archer)
+    assert archer.followers.include?(prova)
+    prova.unfollow(archer)
+    assert_not prova.following?(archer)
+  end
+
+  test "feed should have the right posts" do
+    michael = users(:prova)
+    archer = users(:archer)
+    lana = users(:lana)
+# Posts from followed user
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+# Posts from self
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+# Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
+    end
+  end
+
 end
