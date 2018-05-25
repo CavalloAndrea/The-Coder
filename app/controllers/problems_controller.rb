d3368687109b785672c84d1dd11d70c689936fcc
AndreaCavallo <andreacavallossl@gmail.com>
2018-05-25 18:19:09 +0200

    Creata pagina ricerca esercizi

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,10 +1,25 @@
 class ProblemsController < ApplicationController
+	
+	def index
+		@problems = Problem.paginate(page: params[:page])
+	end
+
 	def new
 		@problem = Problem.new
 	end
 
 	def show
 		@problem = Problem.find(params[:id])
+	end
+
+	def search
+		searchword = params[:search]
+		linguaggio = params[:linguaggio]
+		if linguaggio.blank?
+			@data = Problem.where("((titolo LIKE ?) or (esercizio LIKE ?))", "%#{searchword}%","%#{searchword}%")
+		else
+			@data = Problem.where("((titolo LIKE ?) or (esercizio LIKE ?)) and linguaggio < ?","%#{searchword}%","%#{searchword}%","#{linguaggio}")
+		end
 	end
 
 	def create
--- config/routes.rb
+++ config/routes.rb
@@ -29,14 +29,14 @@ Rails.application.routes.draw do
 
   delete 'logout' => 'sessions#destroy'
 
-  get 'problem' => 'problems#new'
+  get 'search' => 'problems#search'
   
   resources :users do
     member do
       get :following, :followers
     end
   end
-  resources :problems, only:[:create]
+  resources :problems
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only:[:create, :destroy]
